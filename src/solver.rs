88df78f9c0b884b3cfe66d2b578826cbdefa514e
daniel.eades <daniel.eades@hotmail.com>
2022-06-07 11:29:04 +0100

    apply formatting

--- src/solver.rs
+++ src/solver.rs
@@ -1,18 +1,19 @@
-
 pub fn solve(grid: &mut Vec<Vec<u8>>) -> bool {
-
-    if let Some((x, y)) = find_empty(grid) { // If found something
-        for i in 1..10 { // Test every number (1 to 9)
+    if let Some((x, y)) = find_empty(grid) {
+        // If found something
+        for i in 1..10 {
+            // Test every number (1 to 9)
             if is_valid(grid, x, y, i) {
                 grid[y][x] = i;
 
-                if solve(grid) { // If this move is correct return true
+                if solve(grid) {
+                    // If this move is correct return true
                     return true;
                 }
-    
+
                 grid[y][x] = 0; // Else try the next number
             }
-        };
+        }
         false // No number is valid
     } else {
         true // Solved
@@ -27,13 +28,13 @@ fn is_valid(grid: &[Vec<u8>], x: usize, y: usize, n: u8) -> bool {
             return false;
         }
     }
-    
+
     // Check subgrid
     for i in 0..3 {
         for j in 0..3 {
             // Clamp value to between 0 and 2 and multiply it with three
             // Then we will get a subgrid
-            if grid[3*(y/3)+i][3*(x/3)+j] == n {
+            if grid[3 * (y / 3) + i][3 * (x / 3) + j] == n {
                 return false;
             }
         }
